06bd4839a485542f8a3fabf373be3c0692d3ba5b
seongminGang <2544001@a.ut.ac.kr>
2025-03-31 18:44:44 +0900

    저항
    이거 좀 뭔가 익숙한디

---
+++ 1076.py
@@ -0,0 +1,6 @@
+c = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+a = str(c.index(input()))
+b = str(c.index(input()))
+d = str(10**c.index(input()))
+
+print(int(a+b+d[1:]))
